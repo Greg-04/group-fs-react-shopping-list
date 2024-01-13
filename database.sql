49f146f22996c70f9cf2e78af057482768f008a2
Gabriel Regalado <regs0404@gmail.com>
2024-01-13 14:01:04 -0500

    added CRUD routes

--- database.sql
+++ database.sql
@@ -1,2 +1,47 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+-- CREATE TABLE shoppingList (
+-- 	"id" SERIAL PRIMARY KEY,
+-- 	"name" VARCHAR(80) NOT NULL,
+-- 	"quantity" DECIMAL NOT NULL,
+-- 	"unit" VARCHAR(20),
+-- 	"purchased" BOOLEAN DEFAULT FALSE
+-- );
+
+-- INSERT INTO shoppingList ("name", "quantity", "unit", "purchased")
+-- VALUES ();
+
+-- SELECT * FROM shoppingList;
+
+-- Don't forget to add your create table SQL 
+-- It is also helpful to include some test data
+
+CREATE TABLE shoppingList (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"quantity" DECIMAL NOT NULL,
+	"unit" VARCHAR(20),
+	"purchased" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO shoppingList ("name", "quantity", "unit", "purchased")
+VALUES ();
+
+SELECT * FROM shoppingList;
+
+
+--- YING'S DATABASE
+
+CREATE TABLE shopping_list (
+	"id" SERIAL PRIMARY KEY,
+	"name" VARCHAR(80) NOT NULL,
+	"quantity" DECIMAL NOT NULL,
+	"unit" VARCHAR(20),
+	"purchased" BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO shopping_list ("name", "quantity", "unit", "purchased")
+VALUES ();
+
+SELECT * FROM shopping_list;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,29 @@
+// const express = require('express');
+// const app = express();
+// const bodyParser = require('body-parser');
+// const PORT = process.env.PORT || 5001;
+// // const pool = require('pool');
+// const itemRouter = require('./routes/shopping.list.router');
+
+// /** ---------- MIDDLEWARE ---------- **/
+// app.use(bodyParser.json()); // needed for axios requests
+// app.use(express.static('build'));
+
+// /** ---------- EXPRESS ROUTES ---------- **/
+// // Create your API routes in a separate file
+// // and plug them in here with `app.use()`
+// app.use('/database', itemRouter);
+// //app.use('/treats', treats);
+// /** ---------- START SERVER ---------- **/
+// app.listen(PORT,  () => {
+//     console.log('Listening on port: ', PORT);
+// });
+
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5001;
+const itemRouter = require('../server/routes/shopping.list.router');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
@@ -10,6 +32,7 @@ app.use(express.static('build'));
 /** ---------- EXPRESS ROUTES ---------- **/
 // Create your API routes in a separate file
 // and plug them in here with `app.use()`
+app.use('/api/shoppingList', itemRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
